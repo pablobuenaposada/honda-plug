6960ce4ff25849a9476d7ecbf872417a991b2ca4
Pablo Buenaposada <pablo.buenaposada@gmail.com>
2022-10-29 13:04:03 +0200

    add metrics per stock source

--- src/main/views.py
+++ src/main/views.py
@@ -1,5 +1,6 @@
 from django_prometheus import exports
 
+from part.constants import STOCK_SOURCES
 from part.metrics import images, parts, stocks
 from part.models import Image, Part, Stock
 
@@ -9,7 +10,9 @@ def prometheus_override_view(request):
     Override the main Prometheus view to update custom metrics
     """
     parts.set(Part.objects.all().count())
-    stocks.set(Stock.objects.all().count())
+    stocks.labels("all").set(Stock.objects.all().count())
     images.set(Image.objects.all().count())
+    for source in STOCK_SOURCES:
+        stocks.labels(source[0]).set(Stock.objects.filter(source=source[0]).count())
 
     return exports.ExportToDjangoView(request)
--- src/part/metrics.py
+++ src/part/metrics.py
@@ -1,5 +1,5 @@
 from prometheus_client import Gauge
 
 parts = Gauge("parts", "number of total parts")
-stocks = Gauge("stocks", "number of total stocks")
+stocks = Gauge("stocks", "number of total stocks", labelnames=["source"])
 images = Gauge("images", "number of total images")
