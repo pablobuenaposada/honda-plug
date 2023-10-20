e010af1161f74a69866690b1e6d51004996694e8
pablobuenaposada <pablo.buenaposada@gmail.com>
2023-10-21 00:40:50 +0200

    add prometheus info about stocks by user

--- src/main/views.py
+++ src/main/views.py
@@ -1,7 +1,8 @@
 from django_prometheus import exports
 from part.constants import STOCK_SOURCES
-from part.metrics import images, parts, stocks
+from part.metrics import images, parts, stocks, stocks_by_user
 from part.models import Image, Part, Stock
+from rest_framework.authtoken.models import Token
 
 
 def prometheus_override_view(request):
@@ -13,5 +14,9 @@ def prometheus_override_view(request):
     images.set(Image.objects.all().count())
     for source in STOCK_SOURCES:
         stocks.labels(source[0]).set(Stock.objects.filter(source=source[0]).count())
+    for token in Token.objects.all():
+        stocks_by_user.labels(token.user.username).set(
+            Stock.objects.filter(changed_by=token.user).count()
+        )
 
     return exports.ExportToDjangoView(request)
--- src/part/metrics.py
+++ src/part/metrics.py
@@ -3,3 +3,4 @@ from prometheus_client import Gauge
 parts = Gauge("parts", "number of total parts")
 stocks = Gauge("stocks", "number of total stocks", labelnames=["source"])
 images = Gauge("images", "number of total images")
+stocks_by_user = Gauge("stocks_by_user", "number of total stocks", labelnames=["user"])
