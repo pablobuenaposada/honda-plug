3cf0f0f18c6198824fdf06e0713c4c09b9b03871
pablobuenaposada <pablo.buenaposada@gmail.com>
2024-01-08 22:07:31 +0100

    add review part action

--- src/review/admin.py
+++ src/review/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from part.constants import SOURCE_UNKNOWN
+from part.models import Part
 
 from review.models import ReviewPart
 
@@ -6,6 +8,17 @@ from review.models import ReviewPart
 class ReviewPartAdmin(admin.ModelAdmin):
     model = ReviewPart
     list_display = ("reference",)
+    actions = ["add_as_part"]
+
+    def add_as_part(self, request, queryset):
+        references = list(queryset.values_list("reference", flat=True))
+        Part.objects.bulk_create(
+            [
+                Part(reference=reference, source=SOURCE_UNKNOWN)
+                for reference in references
+            ]
+        )
+        queryset.delete()
 
 
 admin.site.register(ReviewPart, ReviewPartAdmin)
