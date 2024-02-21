236c2254beb30141be6f0a14304d75817242e06b
pablobuenaposada <pablo.buenaposada@gmail.com>
2024-02-21 18:21:12 +0100

    make metrics/ endpoint faster

--- src/main/views.py
+++ src/main/views.py
@@ -1,6 +1,6 @@
 from django.contrib.sitemaps import GenericSitemap
+from django.db.models import Count
 from django_prometheus import exports
-from part.constants import STOCK_SOURCES
 from part.documents import StockDocument
 from part.metrics import elasticsearch_stocks, images, parts, stocks, stocks_by_user
 from part.models import Image, Part, Stock
@@ -14,8 +14,8 @@ def prometheus_override_view(request):
     parts.set(Part.objects.all().count())
     stocks.labels("all").set(Stock.objects.all().count())
     images.set(Image.objects.all().count())
-    for source in STOCK_SOURCES:
-        stocks.labels(source[0]).set(Stock.objects.filter(source=source[0]).count())
+    for source_count in Stock.objects.values("source").annotate(count=Count("source")):
+        stocks.labels(source_count["source"]).set(source_count["count"])
     for token in Token.objects.all():
         stocks_by_user.labels(token.user.username).set(
             Stock.objects.filter(changed_by=token.user).count()
